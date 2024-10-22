bd6500d488c3a94a67f29f63611ae8617cc230ba
kevin cuadros <kevindavidcuadros@gmail.com>
2024-10-21 20:51:31 -0500

    Initial Commit

---
+++ Project-6-SwiftUI-animations/Project_6_SwiftUI_animationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_6_SwiftUI_animationsApp.swift
+//  Project-6-SwiftUI-animations
+//
+//  Created by Kevin Cuadros on 21/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Project_6_SwiftUI_animationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
