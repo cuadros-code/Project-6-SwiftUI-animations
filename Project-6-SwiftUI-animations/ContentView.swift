7e250cda022c20b74470494fde3a3b16d612ccc3
kevin cuadros <kevindavidcuadros@gmail.com>
2024-10-22 21:03:49 -0500

    Animating bindings

--- Project-6-SwiftUI-animations/ContentView.swift
+++ Project-6-SwiftUI-animations/ContentView.swift
@@ -10,20 +10,79 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var animationAmount = 1.0
+    
     var body: some View {
-        Button("Tap me") {
-            animationAmount += 1
+        return VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
+            Spacer()
+            Button("Tap me") {
+                animationAmount += 1
+            }
+            .padding(40)
+            .background(.red)
+            .foregroundStyle(.white)
+            .clipShape(.circle)
+            .scaleEffect(animationAmount)
         }
-        .padding(50)
-        .background(.red)
-        .foregroundStyle(.white)
-        .clipShape(.circle)
-        .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount - 1) * 3)
-        .animation(.bouncy, value: animationAmount)
+        
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
+//Button("Tap me") {
+//    animationAmount += 1
+//}
+//.padding(50)
+//.background(.red)
+//.foregroundStyle(.white)
+//.clipShape(.circle)
+//        .scaleEffect(animationAmount)
+//        .blur(radius: (animationAmount - 1) * 3)
+//.animation(
+//    .spring(duration: 1, bounce: 0.5)
+//    .delay(1)
+//    .repeatCount(3, autoreverses: true),
+//    value: animationAmount
+//)
+
+
+
+//Button("Tap me") {
+//    animationAmount += 1
+//}
+//.padding(50)
+//.background(.red)
+//.foregroundStyle(.white)
+//.clipShape(.circle)
+//.overlay(
+//    Circle()
+//        .stroke(.red)
+//        .scaleEffect(animationAmount)
+//        .opacity(2 - animationAmount)
+//        .animation(
+//            .easeOut(duration: 1)
+//            .repeatForever(autoreverses: false),
+//            value: animationAmount
+//        )
+//)
+//.onAppear {
+//    animationAmount = 2
+//}
+
+
+//return VStack {
+//    Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
+//    Spacer()
+//    Button("Tap me") {
+//        animationAmount += 1
+//    }
+//    .padding(40)
+//    .background(.red)
+//    .foregroundStyle(.white)
+//    .clipShape(.circle)
+//    .scaleEffect(animationAmount)
+//}
