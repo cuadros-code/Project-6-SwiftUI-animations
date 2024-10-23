0c7cbe5a69a793b843586ba1a874a8e207108eb6
kevin cuadros <kevindavidcuadros@gmail.com>
2024-10-22 20:39:28 -0500

    implicit animations

--- Project-6-SwiftUI-animations/ContentView.swift
+++ Project-6-SwiftUI-animations/ContentView.swift
@@ -8,8 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var animationAmount = 1.0
     var body: some View {
-        Text("")
+        Button("Tap me") {
+            animationAmount += 1
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        .scaleEffect(animationAmount)
+        .blur(radius: (animationAmount - 1) * 3)
+        .animation(.bouncy, value: animationAmount)
     }
 }
 
