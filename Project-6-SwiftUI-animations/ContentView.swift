7656f1f715ca75b46cca4a84bab6bd9cac69d0b4
kevin cuadros <kevindavidcuadros@gmail.com>
2024-10-24 22:47:23 -0500

    Building custom transitions using ViewModifier

--- Project-6-SwiftUI-animations/ContentView.swift
+++ Project-6-SwiftUI-animations/ContentView.swift
@@ -9,34 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let letters = Array("Kevin Cuadros")
-    @State private var enabled = false
-    @State private var dragAmount = CGSize.zero
-    
-    var body: some View {
-        
-        HStack(spacing: 0) {
-            ForEach(0..<letters.count, id: \.self) { num in
-                Text(String(letters[num]))
-                    .padding(5)
-                    .font(.title)
-                    .background(enabled ? .blue : .red)
-                    .offset(dragAmount)
-                    .animation(
-                        .linear.delay(Double(num) / 20),
-                        value: dragAmount
-                    )
+    @State private var isShowingRed = false
+
+        var body: some View {
+            ZStack {
+                Rectangle()
+                    .fill(.blue)
+                    .frame(width: 200, height: 200)
+
+                if isShowingRed {
+                    Rectangle()
+                        .fill(.red)
+                        .frame(width: 200, height: 200)
+                        .transition(.pivot)
+                }
+            }
+            .onTapGesture {
+                withAnimation {
+                    isShowingRed.toggle()
+                }
             }
         }
-        .gesture(
-            DragGesture()
-                .onChanged { dragAmount = $0.translation }
-                .onEnded { _ in
-                    dragAmount = .zero
-                    enabled.toggle()
-                }
+}
+
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
         )
-        
     }
 }
 
@@ -177,5 +189,62 @@ struct ContentView: View {
 //    )
 //   
 //    .animation(.bouncy, value: dragAmount)
+//    
+//}
+
+
+//let letters = Array("Kevin Cuadros")
+//@State private var enabled = false
+//@State private var dragAmount = CGSize.zero
+//
+//var body: some View {
+//    
+//    HStack(spacing: 0) {
+//        ForEach(0..<letters.count, id: \.self) { num in
+//            Text(String(letters[num]))
+//                .padding(5)
+//                .font(.title)
+//                .background(enabled ? .blue : .red)
+//                .offset(dragAmount)
+//                .animation(
+//                    .linear.delay(Double(num) / 20),
+//                    value: dragAmount
+//                )
+//        }
+//    }
+//    .gesture(
+//        DragGesture()
+//            .onChanged { dragAmount = $0.translation }
+//            .onEnded { _ in
+//                dragAmount = .zero
+//                enabled.toggle()
+//            }
+//    )
+//    
+//}
+
+
+//@State private var isShowingRed = false
+//
+//var body: some View {
+//    
+//    VStack {
+//        Button("Tap me"){
+//            withAnimation {
+//                isShowingRed.toggle()
+//            }
+//        }
+//        
+//        if  isShowingRed {
+//            Rectangle()
+//                .fill(.red)
+//                .frame(width: 200, height: 200)
+//                .transition(
+//                    .asymmetric(insertion: .scale, removal: .opacity)
+//                )
+//        }
+//        
+//      
+//    }
 //    
 //}
